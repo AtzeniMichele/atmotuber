514cd34b6805eb42d7658227d71fbfbccd0f97f1
AtzeniMichele <michele.atzeni@studenti.unipd.it>
2022-07-25 18:22:10 +0200

    'library refactor'

--- lib/atmotuber.dart
+++ lib/atmotuber.dart
@@ -1,3 +1,13 @@
 //library atmotuber;
 
+// library atmotuber
 
+// connector and sync
+export 'package:atmotuber/src/atmotube_ble.dart';
+
+// device info
+export 'package:atmotuber/src/device_info.dart';
+export 'package:atmotuber/src/uart_info.dart';
+
+// data models
+export 'package:atmotuber/src/model.dart';
