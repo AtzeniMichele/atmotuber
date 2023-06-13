2b2402c4b04989a480ee048722735d6c6bfa1794
AtzeniMichele <michele.atzeni@studenti.unipd.it>
2023-06-13 16:08:38 +0200

    minor

--- lib/src/atmotube_ble.dart
+++ lib/src/atmotube_ble.dart
@@ -103,7 +103,8 @@ class Atmotuber {
   /// [searchAtmotubePlus] a method that handles device connection action
   Future<BluetoothDevice?> searchAtmotubePlus([String? id]) async {
     if (completer != null) {
-      completer!.complete;
+      //completer!.complete;
+      completer = null;
     }
     // before all, disconnect any atmotube connected
     await dropConnectionPlus();
